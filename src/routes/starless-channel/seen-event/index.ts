816a1cdd503d9e00cc7ca1e4d42a459999b789d5
Ubuntu <mit@aivm.agvhuxqouo3utetp1u5mtbmk4h.ix.internal.cloudapp.net>
2023-08-08 03:33:06 +0000

    update

---
+++ src/routes/starless-channel/seen-event/index.ts
@@ -0,0 +1,23 @@
+import { brewBlankExpressFunc, throwErrorResponse } from "code-alchemy";
+import log from "../utils/log";
+import connectMongoose from "../../../utils/connect-mongoose";
+import EventHistory from "../../../models/EventHistory";
+
+export default brewBlankExpressFunc(async (req, res) => {
+  const { eventid, userid, accesskey } = req.body;
+  if (accesskey != process.env.access_key) {
+    throwErrorResponse(401, "Unauthorized!");
+  }
+  log(`Seen event api ${eventid} triggered by ${userid}`);
+  await connectMongoose();
+  const eventHistory = await EventHistory.findOne({ eventid, room: userid });
+  if (eventHistory) {
+    eventHistory.status = "seen";
+    await eventHistory.save();
+  }
+
+  res.json({
+    code: 200,
+    message: "Event seen successful.",
+  });
+});
